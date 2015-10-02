d7196f96923a0d56ea827cf13301aa655bad9bc5
Benjamin Nothdurft <b.nothdurft@epages.com>
2015-10-02 02:45:48 +0200

    Remove empty lines

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env sh
-
-
 echo ""
 echo "********************************************************************************"
 echo "  Updating and Installing Prerequisite Packages"
 echo "********************************************************************************"
 echo ""
-
 yum --assumeyes update yum
 yum --assumeyes --exclude=kernel* update
-
 # python-devel is required to build the (optional) C extension for simplejson
 yum --assumeyes install python-pip python-devel
-
-
 echo ""
 echo "********************************************************************************"
 echo "  Installing Ansible"
 echo "********************************************************************************"
 echo ""
-
 yum --assumeyes install ansible
-
-
 echo ""
 echo "********************************************************************************"
 echo "  Finished with bootstrap.sh."
